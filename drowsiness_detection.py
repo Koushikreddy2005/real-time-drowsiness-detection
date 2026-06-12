cf3665fdd2b7564b88f0401e334214a63623b724
koushik reddy chinthala <koushikreddych5@gmail.com>
2026-06-12 10:47:41 +0530

    Update drowsiness_detection.py

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -2,18 +2,17 @@ import cv2
 import mediapipe as mp
 from scipy.spatial import distance
 
-# MediaPipe Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(refine_landmarks=True)
 
-# Open webcam
+
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-# Eye landmark indices
+
 LEFT_EYE = [33, 160, 158, 133, 153, 144]
 RIGHT_EYE = [362, 385, 387, 263, 373, 380]
 
-# Threshold values
+
 EAR_THRESHOLD = 0.25
 FRAME_THRESHOLD = 20
 
@@ -47,27 +46,27 @@ while True:
             left_eye = []
             right_eye = []
 
-            # Left eye points
+           
             for i in LEFT_EYE:
                 x = int(face.landmark[i].x * w)
                 y = int(face.landmark[i].y * h)
                 left_eye.append((x, y))
                 cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
 
-            # Right eye points
+           
             for i in RIGHT_EYE:
                 x = int(face.landmark[i].x * w)
                 y = int(face.landmark[i].y * h)
                 right_eye.append((x, y))
                 cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
 
-            # EAR calculation
+            
             left_ear = eye_aspect_ratio(left_eye)
             right_ear = eye_aspect_ratio(right_eye)
 
             ear = (left_ear + right_ear) / 2.0
 
-            # Show EAR value
+            
             cv2.putText(frame,
                         f"EAR: {ear:.2f}",
                         (20, 50),
@@ -76,7 +75,7 @@ while True:
                         (255, 255, 255),
                         2)
 
-            # Drowsiness detection
+            
             if ear < EAR_THRESHOLD:
                 counter += 1
 
